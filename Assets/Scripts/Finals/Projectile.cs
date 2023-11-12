d8833a092fdb80513ea5961353eb6fb11dccb80a
dyep <geoffrey.mendoza@ciit.edu.ph>
2023-11-12 23:25:50 +0800

    -readjusted projectile to match travel time

--- Assets/Scripts/Finals/Projectile.cs
+++ Assets/Scripts/Finals/Projectile.cs
@@ -12,9 +12,10 @@ public class Projectile : MonoBehaviour {
     }
 
     private IEnumerator Fire_Co(AIController aiCtrl, float damage, float duration = 1f) {
+        Vector3 origPos = transform.position;
         float t = 0;
         for (t = 0 ; t < 1.0f ; t+= Time.deltaTime / duration) {
-            transform.position = Vector3.Lerp(transform.position, aiCtrl.transform.position + Vector3.up, t);
+            transform.position = Vector3.Lerp(origPos, aiCtrl.transform.position + Vector3.up, t);
             yield return new WaitForEndOfFrame();
         }
         aiCtrl.ApplyDamage(damage);
